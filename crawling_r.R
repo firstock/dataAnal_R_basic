b958f482f57ebf3c6a1a83e70866b38d111f78a9
firstock <optimist20@naver.com>
2018-01-22 20:50:40 +0900

    학원 jb51.net data까지

--- crawling_r.R
+++ crawling_r.R
@@ -1,31 +1,56 @@
 #scraping. ?????? ????????????????????? ???????????? ??????
+getwd()
+setwd("d:/")
+# 1 rvest
+install.packages("xml2")
+install.packages("rvest")
+library(xml2)
+library(rvest)
+
+#hyper-link
+url1 <- "http://news.naver.com/"
+sc_url1 <- url1 %>% read_html %>% html_nodes("a") %>% xml_attr("href")
+
+head(sc_url1,8)
+
+#table. https://brunch.co.kr/@dugi/12
+url2 <- "https://en.wikipedia.org/wiki/Student%27s_t-distribution"
+sc_url2 <- url2 %>% read_html %>% html_nodes(".wikitable") %>% html_table()
+head(sc_url2)
+
+#text. http://www.jb51.net/
+url3 <- "http://www.jb51.net/"
+sc_url3 <- url3 %>% read_html %>% html_nodes("a") %>% html_text()
+sc_url4 <- url3 %>% read_html %>% html_nodes("div") %>% html_text()
+head(sc_url3,20)
+summary(sc_url4)
+
+Encoding(sc_url4)
+# sc_url4_gs <- gsub("\\<U\\+[0-9A-Z]{4}\\>","",sc_url4) #?????? ??????& ??????
+writeLines(sc_url4_gs,"data/jb51_4.txt")
 
 # install.packages("scrapeR")
 # install.packages("RCurl")
-install.packages("XML")
-install.packages("bitops")
-library(RCurl)
-library(XML)
-library(bitops)
-
-library(scrapeR)
-??scrapeR
-#
-install.packages("rvest")
-install.packages("xml2")
-library(rvest)
-library(xml2)
-
-
-# url= "http://lumiamitie.github.io/r_tutorial/Rtut03_crawling"
-url= "http://cafe.naver.com/"
-SOURCE= getURL(url)
-PARSED= htmlParse(SOURCE)
-xpathSApply(PARSED, "//*[@id='special-menu-0']",xmlValue)
+# install.packages("XML")
+# install.packages("bitops")
+# library(RCurl)
+# library(XML)
+# library(bitops)
+# 
+# library(scrapeR)
+# ??scrapeR
 
 
 
-web1= readLines('https://feedly.com/i/search/%EC%9B%B9%20%EA%B0%9C%EB%B0%9C/-/-/-/forever/-'
-                # ,encoding="UTF-8"
-                )
-summary(web1)
+# # url= "http://lumiamitie.github.io/r_tutorial/Rtut03_crawling"
+# url= "http://cafe.naver.com/"
+# SOURCE= getURL(url)
+# PARSED= htmlParse(SOURCE)
+# xpathSApply(PARSED, "//*[@id='special-menu-0']",xmlValue)
+# 
+# 
+# 
+# web1= readLines('https://feedly.com/i/search/%EC%9B%B9%20%EA%B0%9C%EB%B0%9C/-/-/-/forever/-'
+#                 # ,encoding="UTF-8"
+#                 )
+# summary(web1)
---
+++ woRdcloud.R
@@ -0,0 +1,44 @@
+getwd()
+setwd("D:/github/dataAnal_R_basic/result")
+
+install.packages("KoNLP") #extractNoun. 설치할렴 jdk필요
+install.packages("RColorBrewer") #brewer.pal
+install.packages("wordcloud") #wordcloud
+library(KoNLP)
+library(RColorBrewer)
+library(wordcloud)
+
+text_jb51_homepage <- readLines("jb51_Eng_utf8.txt")
+txtjb2 <- sapply(text_jb51_homepage, extractNoun, USE.NAMES=F)
+txtjb3 <- unlist(txtjb2)
+txtjb3
+# gsub
+txt <- readLines("w_gsub.txt")
+txt[5]
+cnt_txt <- length(txt)
+for(i in 1:cnt_txt){
+  txtjb3 <- gsub((txt[i]),"",txtjb3)
+}
+
+txtjb3 <- Filter(function(x) {
+  nchar(x)>=2
+}, txtjb3)
+
+# save- I need
+write(unlist(txtjb3),"jb15.net_homepage_text.txt")
+
+# wordcloud- for viewing
+data4 <- read.table("jb15.net_homepage_text.txt")
+wordcount <- table(data4)
+
+#정렬 후 20개만
+head(sort(wordcount, decreasing=T), 20)
+
+palete <- brewer.pal(8,"Set2")
+wordcloud(names(wordcount), freq= wordcount, scale=c(5,1)
+          ,rot.per=0, min.freq=1, random.order=F
+          ,random.color=T, colors=palete)
+
+# ??brewer.pal
+# ??wordcloud
+# RSiteSearch(wordcloud)
