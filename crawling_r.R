245ae67c618f1dc0ea17d615c854ebd9425e2d3a
firstock <optimist20@naver.com>
2018-01-22 18:04:05 +0900

    학원, 크롤링ㄴㄴ스크래핑ㅇㅇ

--- crawling_r.R
+++ crawling_r.R
@@ -1,4 +1,4 @@
-#crawling
+#scraping. ?????? ????????????????????? ???????????? ??????
 
 # install.packages("scrapeR")
 # install.packages("RCurl")
@@ -10,15 +10,22 @@ library(bitops)
 
 library(scrapeR)
 ??scrapeR
+#
+install.packages("rvest")
+install.packages("xml2")
+library(rvest)
+library(xml2)
+
 
 # url= "http://lumiamitie.github.io/r_tutorial/Rtut03_crawling"
-url= "http://cafe.naver.com/dataan#"
+url= "http://cafe.naver.com/"
 SOURCE= getURL(url)
 PARSED= htmlParse(SOURCE)
 xpathSApply(PARSED, "//*[@id='special-menu-0']",xmlValue)
 
 
-#scraping
+
 web1= readLines('https://feedly.com/i/search/%EC%9B%B9%20%EA%B0%9C%EB%B0%9C/-/-/-/forever/-'
-                ,encoding="UTF-8")
+                # ,encoding="UTF-8"
+                )
 summary(web1)
