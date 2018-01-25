9bf75ae7af5a4ff2915217a31b4ec80a878f860e
firstock <optimist20@naver.com>
2018-01-25 20:21:50 +0900

    학원 corr하기전  numeric준비중

--- web_need_easy.R
+++ web_need_easy.R
@@ -11,56 +11,63 @@ kick300 <- head(kick,300)
 str(kick300)
 #같은걸 read.csv로 다시 데려오면 error. 129개 읽어옴
 
-#numeric column only
+## numeric column only
 names(kick300)
-subset(kick300, select=c("deadline","goal","launched"
+kick300_num <- subset(kick300, select=c("deadline","goal","launched"
                   ,"pledged","state","backers","usd pledged"))
 
 # 단일 데이터셋이라 merge 없음
 
 ## state. 범주형 > 숫자화. 1~5
-kick300$state
-stateSort <- unique(kick300$state)
+kick300_num$state
+stateSort <- unique(kick300_num$state)
 head(stateSort,5)
 for(i in 1:length(stateSort)){
-  kick300$state <- gsub(stateSort[i],i,kick300$state)
+  kick300_num$state <- gsub(stateSort[i],i,kick300_num$state)
 }
 
-head(kick300,1)
-names(kick300)
-str(kick300)
+head(kick300_num,1)
+names(kick300_num)
+str(kick300_num)
 
-# error! FUN(newX[, i], ...) : invalid multibyte string, element 64##### 이상치 간단 체크. 칼럼별 celㅣ 최대 길이 보기> 폴(i in 1:length(kick300)) {print(max(data.frame(chr = apply(kick300, 2, nchar)[, i]))) }
-# error! deadline을 다 NA로 밀어버림, goal 9자리 무엇? error! filter랑 sapply랑 의미 공부!. 딴거 <- sapply(kick300$goal, function(x) {Filter(function(y) {nchar(as.character(y)) == 9 }, x) })
+## as.numeric
+
+
+
+## 이상치?
+# error! FUN(newX[, i], ...) : invalid multibyte string, element 64##### 이상치 간단 체크. 칼럼별 celㅣ 최대 길이 보기> 폴(i in 1:length(kick300_num)) {print(max(data.frame(chr = apply(kick300_num, 2, nchar)[, i]))) }
+# error! deadline을 다 NA로 밀어버림, goal 9자리 무엇? error! filter랑 sapply랑 의미 공부!. 딴거 <- sapply(kick300_num$goal, function(x) {Filter(function(y) {nchar(as.character(y)) == 9 }, x) })
 
 # install.packages("lubridate")
-kick300$deadline
+kick300_num$deadline
 
 library(lubridate)
-kick300$deadline_hm <- ymd_hms(kick300$deadline)
-kick300$launched_hm <- ymd_hms(kick300$launched)
-head(kick300$deadline)
-head(kick300$launched)
+kick300_num$deadline_hm <- ymd_hms(kick300_num$deadline)
+kick300_num$launched_hm <- ymd_hms(kick300_num$launched)
+head(kick300_num$deadline)
+head(kick300_num$launched)
 
 ## date -> 정수형 파생변수 만들기
-kick300$endY <- year(kick300$deadline)
-kick300$endMon <- month(kick300$deadline)
-kick300$endD <- day(kick300$deadline)
-kick300$endH <- hour(kick300$deadline)
-kick300$endMin <- minute(kick300$deadline)
-kick300$endS <- second(kick300$deadline)
+kick300_num$endY <- year(kick300_num$deadline)
+kick300_num$endMon <- month(kick300_num$deadline)
+kick300_num$endD <- day(kick300_num$deadline)
+kick300_num$endH <- hour(kick300_num$deadline)
+kick300_num$endMin <- minute(kick300_num$deadline)
+kick300_num$endS <- second(kick300_num$deadline)
 
 
-kick300$initY <- year(kick300$launched)
-kick300$initMon <- month(kick300$launched)
-kick300$initD <- day(kick300$launched)
-kick300$initH <- hour(kick300$launched)
-kick300$initMin <- minute(kick300$launched)
-kick300$initS <- second(kick300$deadline)
+kick300_num$initY <- year(kick300_num$launched)
+kick300_num$initMon <- month(kick300_num$launched)
+kick300_num$initD <- day(kick300_num$launched)
+kick300_num$initH <- hour(kick300_num$launched)
+kick300_num$initMin <- minute(kick300_num$launched)
+kick300_num$initS <- second(kick300_num$deadline)
 
-names(kick300)
-head(kick300,2)
-str(kick300)
+names(kick300_num)
+head(kick300_num,2)
+str(kick300_num)
+summary(kick300_num)
+
 ## 가설: 목표금액이 높을 수록 프로젝트 성공률이 낮다. goal is smaller than, state will be fail
 ## 종속변수_state ~ 독립변수_goal + pledged+ backers+ usd.pledged
 # install.packages("caret")
@@ -70,10 +77,14 @@ library(caret)
 library(corrplot)
 library(FactoMineR)
 
-# 상관관계
-mod <- lm(state~ ., data=kick300)
-plot(mod)
+# 상관관계. 종속변수가 factor라서 로지스틱 회귀 써야.
+kick300_num$state <- as.factor(kick300_num$state)
+str(kick300_num)
+
+mod <- lm(state~ goal, data=kick300_num)
+summary(mod)
+
 
 #error !
-cor <- cor(round(kick300[,],digit=0))
+cor <- cor(round(kick300_num[,],digit=0))
 
